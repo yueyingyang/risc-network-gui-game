b2028184f505149f8ff6a9d37dfab222ba3092c8
Yueying <yangyueying22@gmail.com>
2021-03-07 20:44:27 -0500

    update testserver

--- client/src/test/java/edu/duke/ece651/risc/client/TestServer.java
+++ client/src/test/java/edu/duke/ece651/risc/client/TestServer.java
@@ -1,2 +1,37 @@
-package edu.duke.ece651.risc.client;public class testServer {
+package edu.duke.ece651.risc.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TestServer {
+    private final ServerSocket serverSocket;
+    private final Socket clientSocket;
+    private final PrintWriter out;
+    private final BufferedReader in;
+
+    public TestServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void sendMessage(String msg) {
+        out.println(msg);
+    }
+
+    public String recvMessage() throws IOException {
+        return in.readLine();
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 }
