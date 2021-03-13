7cf973ad30dba762e49e6d3215ede876e81d4e2c
tong shen <ts325@duke.edu>
2021-03-13 03:01:53 -0800

    add Test

--- shared/src/test/java/edu/duke/ece651/risc/shared/MoveEntryTest.java
+++ shared/src/test/java/edu/duke/ece651/risc/shared/MoveEntryTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AttackEntryTest {
+class MoveEntryTest {
 
     @Test
     public void test_apply() {
@@ -15,13 +15,17 @@ class AttackEntryTest {
         List<String> names = Arrays.asList("LiLei", "HanMeiMei");
         GameMap myMap = f.createMap(names, 2);
         ActionEntry entry0 = new PlaceEntry("0", 8);
-        ActionEntry entry1 = new AttackEntry("0", "2", 3);
-        entry0.apply(myMap);
-        entry1.apply(myMap);
+        ActionEntry entry1 = new PlaceEntry("1", 3);
+        ActionEntry entry2 = new MoveEntry("0", "1", 2);
+        ActionEntry[] entries = {entry0, entry1, entry2};
+        for (ActionEntry entry : entries) {
+            entry.apply(myMap);
+        }
         Territory terr0 = myMap.getTerritory("0");
-        Territory terr1 = myMap.getTerritory("2");
-        assertEquals(5, terr0.getNumSoldiersInArmy());
-        assertEquals(3, terr1.getNumSoldiersInAttacker("LiLei"));
+        Territory terr1 = myMap.getTerritory("1");
+        assertEquals(6, terr0.getNumSoldiersInArmy());
+        assertEquals(5, terr1.getNumSoldiersInArmy());
     }
+
 
 }
