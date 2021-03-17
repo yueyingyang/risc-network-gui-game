779c087c8090d23d38012c51cba6744abd9ef319
tong shen <ts325@duke.edu>
2021-03-17 15:55:20 -0700

    add belongToSameOwner to Checker

--- shared/src/main/java/edu/duke/ece651/risc/shared/Checker.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Checker.java
@@ -1,18 +1,18 @@
 package edu.duke.ece651.risc.shared;
 
 public abstract class Checker {
-  private final Checker next;
+    private final Checker next;
 
-  public Checker(Checker next){
-    this.next = next;
-  }
-
-  protected abstract void checkMyRule(ActionEntry action, GameMap map);
-  
-  public void checkAction(ActionEntry action, GameMap map){
-    checkMyRule(action,map);
-    if(next!=null){
-      next.checkAction(action,map);
+    public Checker(Checker next) {
+        this.next = next;
     }
-  }
+
+    protected abstract void checkMyRule(ActionEntry action, GameMap map);
+
+    public void checkAction(ActionEntry action, GameMap map) {
+        checkMyRule(action, map);
+        if (next != null) {
+            next.checkAction(action, map);
+        }
+    }
 }
--- shared/src/main/java/edu/duke/ece651/risc/shared/ClientChecker.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/ClientChecker.java
@@ -1,20 +1,20 @@
 package edu.duke.ece651.risc.shared;
 
-public class ClientChecker extends Checker{
-    public ClientChecker(Checker next){
+public class ClientChecker extends Checker {
+    public ClientChecker(Checker next) {
         super(next);
     }
 
-    public void checkMyRule(ActionEntry action, GameMap map){
-        if(map.getTerritory(action.getFromName())==null){
+    public void checkMyRule(ActionEntry action, GameMap map) {
+        if (map.getTerritory(action.getFromName()) == null) {
             throw new IllegalArgumentException("The start territory name is invalid!");
         }
-        if(map.getTerritory(action.getToName())==null){
+        if (map.getTerritory(action.getToName()) == null) {
             throw new IllegalArgumentException("The destination territory name is invalid!");
         }
-        Territory start=map.getTerritory(action.getFromName());
+        Territory start = map.getTerritory(action.getFromName());
 
-        if(start.getNumSoldiersInArmy()<action.getNumSoldiers()){
+        if (start.getNumSoldiersInArmy() < action.getNumSoldiers()) {
             throw new IllegalArgumentException("The army in this territory is not enough for this action!\n");
         }
     }
