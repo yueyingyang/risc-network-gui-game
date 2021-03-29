133c5c0fb0472ba951a6da611cab98c251ff44d2
Yueying <yangyueying22@gmail.com>
2021-03-29 17:11:58 -0400

    add comments

--- web/src/main/java/edu/duke/ece651/risc/web/GameController.java
+++ web/src/main/java/edu/duke/ece651/risc/web/GameController.java
@@ -1,12 +1,14 @@
 package edu.duke.ece651.risc.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.duke.ece651.risc.shared.*;
 import edu.duke.ece651.risc.shared.game.TerrUnit;
 import edu.duke.ece651.risc.shared.game.TerrUnitList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,74 +23,96 @@ public class GameController {
 
   // todo: change after user login
   private String currentUserName;
+  // need update after switch rooms
+  private List<String> players;
+
+  // utils
   private final ObjectMapper mapper;
   private final JSONSerializer jsonSerializer;
+  private final List<String> colorPalette;
+
 
   public GameController() {
     this.jsonSerializer = new JSONSerializer();
     this.currentUserName = "test";
     this.mapper = new ObjectMapper();
+    this.colorPalette = Arrays.asList("#97B8A3", "#EDC3C7", "#FDF06F", "#A6CFE2", "#9C9CDD");
+    this.players = Arrays.asList("p2", "test");
   }
 
+  /**
+   * The game page: after enter
+   *
+   * @param userName for temporary usage, should be removed after adding user login
+   * @param model    is the model to add attribute for view to display
+   * @return is the name of view template html
+   * @throws IOException
+   */
   @GetMapping("/game")
   public String greeting(@RequestParam(value = "name") String userName, Model model) throws IOException {
-
-    List<String> colorPalette = Arrays.asList("#97B8A3", "#EDC3C7", "#FDF06F", "#A6CFE2", "#9C9CDD");
-    List<String> players = Arrays.asList("p2", "test");
-    Map<String, String> colorMapping = new HashMap<>();
-    for (int i = 0; i < players.size(); i++) {
-      colorMapping.put(players.get(i), colorPalette.get(i));
-    }
 //    GameMap map = (GameMap) jsonSerializer.deserialize(playerMapping.getClientPlayer(userName).recvMessage(), GameMap.class);
 //    int totalUnits = Integer.parseInt(playerMapping.getClientPlayer(userName).recvMessage());
     GameMap map = createMap();
     int totalUnits = 6;
-
-    List<ObjectNode> graphData = getObjectNodes(colorMapping, map);
+    List<ObjectNode> graphData = getObjectNodes(map, players);
     model.addAttribute("graphData", graphData);
-    model.addAttribute("wrapper", createTerriUnitList(map, userName));
+    model.addAttribute("wrapper", createTerrUnitList(map, userName));
     model.addAttribute("units", totalUnits);
     return "game";
   }
 
+  /**
+   * Placement submission API
+   *
+   * @param list  is the TerrUnitList user input
+   * @param model idk if it's needed
+   * @return redirect to game page
+   * @throws IOException if recv/send exception
+   */
   @PostMapping(value = "/place")
   public String place(@ModelAttribute(value = "wrapper") TerrUnitList list, Model model) throws IOException {
     List<ActionEntry> placementList = new ArrayList<>();
-    for(TerrUnit tu : list.getTerrUnitList()){
-      System.out.println(tu.getTerrName());
+    ClientSocket cs = playerMapping.getSocket(currentUserName);
+    for (TerrUnit tu : list.getTerrUnitList()) {
+      placementList.add(new PlaceEntry(tu.getTerrName(), tu.getUnit(), currentUserName));
       System.out.println(tu.getUnit());
     }
-
-//    placements.add()
-//    c.sendMessage(new ObjectMapper().writeValueAsString(startReq));
-//    placementList.add(new PlaceEntry(t.getName(), num, getName()));
-
-//    playerMapping.getClientPlayer("test").sendMessage(jsonSerializer.getOm().writerFor(new TypeReference<List<ActionEntry>>() {
-//    }).writeValueAsString(placementList));
-    model.addAttribute("wrapper", list);
+    playerMapping.getSocket(currentUserName).sendMessage(jsonSerializer.getOm().writerFor(new TypeReference<List<ActionEntry>>() {
+    }).writeValueAsString(placementList));
     return "game";
   }
 
-//  @GetMapping(value = "/update_map")
-//  public @ResponseBody
-//  List<ObjectNode> tryUpdateMap() {
-//    if(playerMapping.getClientPlayer().getInputStream())
-//  }
+  /**
+   * Ajax GET API for update map after placement
+   *
+   * @return Response with status error or success, if success then body is updated GAMEMAP
+   * @throws IOException
+   */
+  @GetMapping(value = "/update_map")
+  public @ResponseBody
+  ResponseEntity<?> tryUpdateMap() throws IOException {
+    ClientSocket cs = playerMapping.getSocket(currentUserName);
+    if (cs.hasNewMsg()) {
+      GameMap map = (GameMap) jsonSerializer.deserialize(cs.recvMessage(), GameMap.class);
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getObjectNodes(map, players));
+    }
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+  }
 
-  // Below is helper functions
+  // Below are helper functions
 
   /**
-   * Parse GameMap as JSON Node for Map display
-   * source:[
-   * {"owner":"Red", "resources":"10", "value":"2"},
-   * {"owner":"Red", "resources":"10", "value":"2"},
-   * ]
+   * Convert GameMap to the JSON NODE to display map
    *
-   * @param colorMapping
-   * @param map
-   * @return
+   * @param map     is map to be converted
+   * @param players is the player list to do the color mapping
+   * @return the MAP display info in JSON
    */
-  private List<ObjectNode> getObjectNodes(Map<String, String> colorMapping, GameMap map) {
+  private List<ObjectNode> getObjectNodes(GameMap map, List<String> players) {
+    Map<String, String> colorMapping = new HashMap<>();
+    for (int i = 0; i < players.size(); i++) {
+      colorMapping.put(players.get(i), colorPalette.get(i));
+    }
     List<ObjectNode> graphData = new ArrayList<>();
     for (Territory t : map.getAllTerritories()) {
       ObjectNode o = mapper.createObjectNode();
@@ -102,27 +126,40 @@ public class GameController {
     return graphData;
   }
 
-
-  //  mock a map
-  private GameMap createMap() {
-    V1MapFactory v1f = new V1MapFactory();
-    List<String> players = Arrays.asList("test", "p2");
-    GameMap map = v1f.createMap(players, 2);
-//    List<ActionEntry> pl = Arrays.asList(new PlaceEntry("0", 2, "test"),
-//            new PlaceEntry("1", 2, "test"),
-//            new PlaceEntry("2", 2, "player2"),
-//            new PlaceEntry("3", 2, "player2"));
-//    for (ActionEntry ae : pl) {
-//      ae.apply(map);
-//    }
-    return map;
-  }
-
-  protected TerrUnitList createTerriUnitList(GameMap map, String userName) {
+  /**
+   * Wrap terrUnit for placement phase
+   *
+   * @param map      is the map to be placed on
+   * @param userName is the current user name, for extracting it's territory
+   * @return the TerrUnitList for MVC
+   */
+  protected TerrUnitList createTerrUnitList(GameMap map, String userName) {
     List<TerrUnit> ans = new ArrayList<>();
     for (Territory t : map.getPlayerTerritories(userName)) {
       ans.add(new TerrUnit(t.getName(), 0));
     }
     return new TerrUnitList(ans);
+  }
+
+
+  /**
+   * There are many places need a map to test...
+   *
+   * @return a GameMap object
+   */
+  private GameMap createMap() {
+    V1MapFactory v1f = new V1MapFactory();
+    List<String> players = Arrays.asList("test", "p2");
+    Collections.shuffle(players);
+    return v1f.createMap(players, 2);
+  }
+
+  /**
+   * For update player list after switching game room
+   *
+   * @param players is the updated player list
+   */
+  public void setPlayers(List<String> players) {
+    this.players = players;
   }
 }
