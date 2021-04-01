e5f2148d33cfd6b84f42d185b10eaea27d791104
tong shen <ts325@duke.edu>
2021-04-01 12:28:09 -0700

    add Constant and GameUtil

--- shared/src/main/java/edu/duke/ece651/risc/shared/Constant.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/Constant.java
@@ -73,37 +73,5 @@ public class Constant {
         soldierCost.put(5, 90);
         soldierCost.put(6, 140);
     }
-
-    /**
-     * Get the bonus of the soldier at the indicated level
-     *
-     * @param level is the indicated level
-     * @return the bonus
-     */
-    public static int getBonusAtLevel(int level) {
-        return bonus.get(level);
-    }
-
-    /**
-     * Get the cost to upgrade tech level by one from current level
-     *
-     * @param currLevel is the current technology level
-     * @return the cost to upgrade the technology
-     */
-    public static int getTechCost(int currLevel) {
-        return techCost.get(currLevel);
-    }
-
-    /**
-     * Get the cost to upgrade soldiers
-     *
-     * @param fromLevel   is the current level of the soldiers
-     * @param toLevel     is the level of the soldiers after upgrade
-     * @param numSoldiers is the number of soldiers
-     * @return the cost to upgrade soldiers
-     */
-    public static int getSoldierCost(int fromLevel, int toLevel, int numSoldiers) {
-        return numSoldiers * (soldierCost.get(toLevel) - soldierCost.get(fromLevel));
-    }
 }
 
---
+++ shared/src/test/java/edu/duke/ece651/risc/shared/GameUtilTest.java
@@ -0,0 +1,32 @@
+package edu.duke.ece651.risc.shared;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameUtilTest {
+
+    @Test
+    public void test_getBonusAtLevel() {
+        int[] expect = {0, 1, 3, 5, 8, 11, 15};
+        for (int i = 0; i < 7; i++) {
+            assertEquals(expect[i], GameUtil.getBonusAtLevel(i));
+        }
+    }
+
+    @Test
+    public void test_getTechCost() {
+        int[] expect = {0, 50, 75, 125, 200, 300};
+        for (int i = 1; i < 6; i++) {
+            assertEquals(expect[i], GameUtil.getTechCost(i));
+        }
+    }
+
+    @Test
+    public void test_getSoldierCost() {
+        assertEquals(237 , GameUtil.getSoldierCost(2, 5, 3));
+        assertEquals(170, GameUtil.getSoldierCost(4, 6, 2));
+        assertEquals(108, GameUtil.getSoldierCost(1, 3, 4));
+    }
+
+}
