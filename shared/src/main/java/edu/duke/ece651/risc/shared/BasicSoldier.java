061220bf31a419640e0c02a25ab63c83fa2ea581
tong shen <ts325@duke.edu>
2021-04-01 13:14:16 -0700

    add type to Soldier

--- shared/src/main/java/edu/duke/ece651/risc/shared/BasicSoldier.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/BasicSoldier.java
@@ -4,8 +4,27 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Random;
 
+/**
+ * A class represents a basic soldier
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BasicSoldier implements Soldier {
+
+    private String type;
+
+    /**
+     * Construct a Basic Soldier Object
+     */
+    public BasicSoldier(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Construct a Basic Soldier Object
+     */
+    public BasicSoldier() {
+        this("0");
+    }
 
     /**
      * Fight with the attacker
@@ -22,4 +41,24 @@ public class BasicSoldier implements Soldier {
         int attackerRoll = myRandom.nextInt(length);
         return defenderRoll - attackerRoll;
     }
+
+    /**
+     * Upgrade a soldier to the indicated type
+     *
+     * @param toType is the indicated type
+     */
+    public void upgrade(String toType) {
+        type = toType;
+    }
+
+    /**
+     * Check whether a soldier has the indicated type
+     *
+     * @param myType is the indicated type
+     * @return true if the soldier is has the indicated type else false
+     */
+    public boolean hasType(String myType) {
+        return type.equals(myType);
+    }
+
 }
