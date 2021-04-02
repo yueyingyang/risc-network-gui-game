5fd8e1335860d7d5c1120eaeebc2c09541228a1d
tong shen <ts325@duke.edu>
2021-04-01 20:43:47 -0700

    add doc

--- shared/src/main/java/edu/duke/ece651/risc/shared/PlayerInfo.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/PlayerInfo.java
@@ -11,7 +11,7 @@ public class PlayerInfo {
     private int techResource;
 
     /**
-     * Construct a playerInfo object
+     * Construct a PlayerInfo object
      *
      * @param techLevel    is the technology level
      * @param foodResource is the number of food
@@ -24,34 +24,62 @@ public class PlayerInfo {
     }
 
     /**
-     * Construct a playerInfo object
+     * Construct a PlayerInfo object
      */
     public PlayerInfo() {
         this(1, 0, 0);
     }
 
+    /**
+     * Upgrade technology by one level
+     */
     public void upgradeTech() {
         int cost = GameUtil.getTechCost(techLevel);
         consumeTech(cost);
         techLevel += 1;
     }
 
+    /**
+     * Consume technology resource
+     *
+     * @param cost is the technology cost
+     */
     public void consumeTech(int cost) {
         techResource -= cost;
     }
 
+    /**
+     * Consume the food resource
+     *
+     * @param cost is the food cost
+     */
     public void consumeFood(int cost) {
         foodResource -= cost;
     }
 
+    /**
+     * Get the technology level of the player
+     *
+     * @return the technology level
+     */
     public int getTechLevel() {
         return techLevel;
     }
 
+    /**
+     * Get the number of food resource
+     *
+     * @return the number of food resource
+     */
     public int getFoodResource() {
         return foodResource;
     }
 
+    /**
+     * Get the number of technology resource
+     *
+     * @return the number of technology resource
+     */
     public int getTechResource() {
         return techResource;
     }
