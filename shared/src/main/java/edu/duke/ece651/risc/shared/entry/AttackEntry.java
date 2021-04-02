a36ad32c35cf111a348036997c15def4f0659d1e
tong shen <ts325@duke.edu>
2021-04-02 12:50:56 -0700

    revert to previous version

--- shared/src/main/java/edu/duke/ece651/risc/shared/entry/AttackEntry.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/entry/AttackEntry.java
@@ -16,12 +16,11 @@ public class AttackEntry extends BasicEntry {
      * @param toName      is the name of the to-territory
      * @param numSoldiers is the number of soldiers
      * @param playerName  is the name of the player
-     * @param fromType    is the type of the soldier participating in the attack
      * @annotation ConstructorProperties is for JSON deserialization
      */
-    @ConstructorProperties({"fromName", "toName", "numSoldiers", "playerName", "fromType"})
-    public AttackEntry(String fromName, String toName, int numSoldiers, String playerName, String fromType) {
-        super(fromName, toName, numSoldiers, playerName, fromType, null);
+    @ConstructorProperties({"fromName", "toName", "numSoldiers", "playerName"})
+    public AttackEntry(String fromName, String toName, int numSoldiers, String playerName) {
+        super(fromName, toName, numSoldiers, playerName, null, null);
     }
 
     /**
@@ -36,8 +35,8 @@ public class AttackEntry extends BasicEntry {
         myChecker.checkAction(this, myMap);
         Territory fromTerr = myMap.getTerritory(fromName);
         Territory toTerr = myMap.getTerritory(toName);
-        fromTerr.removeSoldiersFromArmy(numSoldiers, fromType);
-        Army attacker = new BasicArmy(fromTerr.getOwnerName(), numSoldiers, fromType);
+        fromTerr.removeSoldiersFromArmy(numSoldiers);
+        Army attacker = new BasicArmy(fromTerr.getOwnerName(), numSoldiers);
         toTerr.bufferAttacker(attacker);
     }
 }
