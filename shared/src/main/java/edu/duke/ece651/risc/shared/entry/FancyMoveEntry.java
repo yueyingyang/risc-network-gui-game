9f81510f354cbac21672d80fbe90b6f33c9f79ef
tong shen <ts325@duke.edu>
2021-04-04 18:29:34 -0700

    add checker

--- shared/src/main/java/edu/duke/ece651/risc/shared/entry/FancyMoveEntry.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/entry/FancyMoveEntry.java
@@ -3,6 +3,9 @@ package edu.duke.ece651.risc.shared.entry;
 import edu.duke.ece651.risc.shared.GameMap;
 import edu.duke.ece651.risc.shared.PlayerInfo;
 import edu.duke.ece651.risc.shared.Territory;
+import edu.duke.ece651.risc.shared.checker.Checker;
+import edu.duke.ece651.risc.shared.checker.ClientChecker;
+import edu.duke.ece651.risc.shared.checker.FancyMoveRuleChecker;
 
 import java.beans.ConstructorProperties;
 
@@ -34,6 +37,8 @@ public class FancyMoveEntry extends BasicEntry {
      */
     @Override
     public void apply(GameMap myMap, PlayerInfo myInfo) {
+        Checker myChecker = new ClientChecker(new FancyMoveRuleChecker(null));
+        myChecker.checkAction(this, myMap, myInfo);
         Territory fromTerr = myMap.getTerritory(fromName);
         Territory toTerr = myMap.getTerritory(toName);
         fromTerr.removeSoldiersFromArmy(numSoldiers, fromType);
