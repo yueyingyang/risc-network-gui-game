487a07d01d0b48c4ed88f5e56bdf1c73c15ab872
tong shen <ts325@duke.edu>
2021-04-17 00:17:05 -0700

    add spy entry

---
+++ shared/src/main/java/edu/duke/ece651/risc/shared/entry/SpyEntry.java
@@ -0,0 +1,37 @@
+package edu.duke.ece651.risc.shared.entry;
+
+import edu.duke.ece651.risc.shared.GameMap;
+import edu.duke.ece651.risc.shared.PlayerInfo;
+import edu.duke.ece651.risc.shared.Territory;
+
+/**
+ * An entry used to upgrade type-0 soldiers to spies
+ */
+public class SpyEntry extends BasicEntry {
+
+    /**
+     * Construct a SpyEntry object
+     *
+     * @param toName      is the name of the territory
+     * @param numSoldiers is the number of soldiers
+     * @param playerName  is the player name
+     */
+    public SpyEntry(String toName, int numSoldiers, String playerName) {
+        super(null, toName, numSoldiers, playerName, null, null);
+    }
+
+    /**
+     * Apply the action on the action entry
+     *
+     * @param myMap  is the map of the game
+     * @param myInfo is the player info
+     */
+    @Override
+    public void apply(GameMap myMap, PlayerInfo myInfo) {
+        Territory terr = myMap.getTerritory(toName);
+        terr.removeSoldiersFromArmy(numSoldiers);
+        terr.addSpies(numSoldiers);
+        int cost = numSoldiers * 20;
+        myInfo.consumeTech(cost);
+    }
+}
