efe3a4665b1ad4d19493f3a633bff1e060e2f774
tong shen <ts325@duke.edu>
2021-04-17 20:54:30 -0700

    add docs

--- shared/src/main/java/edu/duke/ece651/risc/shared/entry/SpyMoveEntry.java
+++ shared/src/main/java/edu/duke/ece651/risc/shared/entry/SpyMoveEntry.java
@@ -4,12 +4,29 @@ import edu.duke.ece651.risc.shared.GameMap;
 import edu.duke.ece651.risc.shared.PlayerInfo;
 import edu.duke.ece651.risc.shared.Territory;
 
+/**
+ * An entry to move the spy
+ */
 public class SpyMoveEntry extends BasicEntry {
+    /**
+     * Construct a SpyMoveEntry
+     *
+     * @param fromName    is the name of the from-territory
+     * @param toName      is the name of the to-territory
+     * @param numSoldiers is the number of soldiers
+     * @param playerName  is the player name
+     */
     public SpyMoveEntry(String fromName, String toName, int numSoldiers,
                         String playerName) {
         super(fromName, toName, numSoldiers, playerName, "0", null);
     }
 
+    /**
+     * Apply the entry
+     *
+     * @param myMap  is the map of the game
+     * @param myInfo is the player info
+     */
     @Override
     public void apply(GameMap myMap, PlayerInfo myInfo) {
         Territory fromTerr = myMap.getTerritory(fromName);
